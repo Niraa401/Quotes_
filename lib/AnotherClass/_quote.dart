6a06ca4322ab7a4f75e4b99bac06d61fa9782ec9
NIRALI SAVALIYA <savaliyanirali41@gmail.com>
2023-02-14 13:27:07 +0530

    Delete _quote.dart

--- lib/AnotherClass/_quote.dart
+++
@@ -1,8 +0,0 @@
-class Quote {
-  // ignore: prefer_typing_uninitialized_variables
-  var author;
-  // ignore: prefer_typing_uninitialized_variables
-  var text;
-
-  Quote({this.author, this.text});
-}
